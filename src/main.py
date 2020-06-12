580d250c1021f382a54f5eba806cabf43cdcfcf3
DimitriosApazidis <da039@hdm-stuttgart.de>
2020-06-12 14:41:07 +0200

    Updated main.py Updated User List Operations

--- src/main.py
+++ src/main.py
@@ -85,3 +85,144 @@ class GroupListOperations(Resource):
 
 @ikauf.route('/retailer')
 @ikauf.response(500)
+
+
+
+@ikauf.route('/user/<int:id>')
+@ikauf.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@ikauf.param('id', 'Die ID des User-Objekts')
+class UserOperations(Resource):
+    @ikauf.marshal_with(user)
+    @secured
+    def get(self, id):
+        """Auslesen eines bestimmten User-Objekts.
+
+        Das auszulesende Objekt wird durch die ```id``` in dem URI bestimmt.
+        """
+        adm = ShoppingListAdministration()
+        cust = adm.get_user_by_id(id)
+        return cust
+
+    @secured
+    def delete(self, id):
+        """Löschen eines bestimmten User-Objekts.
+
+        Das zu löschende Objekt wird durch die ```id``` in dem URI bestimmt.
+        """
+        adm = ShoppingListAdministration()
+        cust = adm.get_user_by_id(id)
+        adm.delete_user(cust)
+        return '', 200
+
+    @ikauf.marshal_with(user)
+    @ikauf.expect(user, validate=True)
+    @secured
+    def put(self, id):
+        """Update eines bestimmten User-Objekts.
+
+        **ACHTUNG:** Relevante id ist die id, die mittels URI bereitgestellt und somit als Methodenparameter
+        verwendet wird. Dieser Parameter überschreibt das ID-Attribut des im Payload der Anfrage übermittelten
+        Customer-Objekts.
+        """
+        adm = ShoppingListAdministration()
+        c = user.from_dict(api.payload)
+
+        if c is not None:
+            """Hierdurch wird die id des zu überschreibenden (vgl. Update) Customer-Objekts gesetzt.
+            Siehe Hinweise oben.
+            """
+            c.set_id(id)
+            adm.save_user(c)
+            return '', 200
+        else:
+            return '', 500
+
+
+@ikauf.route('/user-by-name/<string:name>')
+@ikauf.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@ikauf.param('name', 'Der Name des Kunden')
+class UserByNameOperations(Resource):
+    @ikauf.marshal_with(user)
+    @secured
+    def get(self,name):
+        """ Auslesen von User-Objekten, die durch den Namen bestimmt werden.
+
+        Die auszulesenden Objekte werden durch ```name``` in dem URI bestimmt.
+        """
+        adm = ShoppingListAdministration()
+        cust = adm.get_user_by_name(name)
+        return cust
+
+
+@ikauf.route('/user-by-external-id/<int:external_id>')
+@ikauf.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@ikauf.param('external_id', 'Die Id des User-Objektes')
+
+class UserByExternalIdOperations(Resource):
+    @ikauf.marshal_with(user)
+    @secured
+    def get(self,external_id):
+        """ Auslesen von User-Objekten, die durch eine externe Id bestimmt werden.
+
+        Die auszulesenden Objekte werden durch ```external_id``` in dem URI bestimmt.
+        """
+        adm = ShoppingListAdministration()
+        cust = adm.get_user_by_external_id(external_id)
+        return cust
+
+@ikauf.route('/user-by-email/<string:email>')
+@ikauf.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@ikauf.param('email', 'Die Email des User')
+class UserByEmailOperations(Resource):
+    @ikauf.marshal_with(user)
+    @secured
+    def get(self,email):
+        """ Auslesen von User-Objekten, die durch die E-Mail bestimmt werden.
+
+        Die auszulesenden Objekte werden durch ```email``` in dem URI bestimmt.
+        """
+        adm = ShoppingListAdministration()
+        cust = adm.get_user_by_email(email)
+        return cust
+
+@ikauf.route('/group-by-user/int:id>')  String? Oder was???  GroupId UserId
+@ikauf.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+@ikauf.param('userId', 'Das User Objekt')
+class GroupByUserOperations(Resource):
+    @ikauf.marshal_with(user)
+    @secured
+    def get(self,user):
+        """ Auslesen von Gruppen-Objekten, die durch den User bestimmt werden.
+
+        Die auszulesenden Objekte werden durch ```user``` in dem URI bestimmt.
+        """
+        adm = ShoppingListAdministration()
+        cust = adm.get_group_by_user(user)
+        return cust
+
+
+
+@ikauf.route('/user-by-list')   ???
+@ikauf.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
+class UserListOperations(Resource):
+  @ikauf.marshal_list_with(user)
+  @secured
+  def get(self):
+      "Auslesen aller User-Objekte"
+
+      adm = ShoppingListAdministration()
+      user = adm.get_all_user()
+      return user
+
+  def post(self):
+    "Anlegen eines neuen User-Objekts"
+
+    adm = ShoppingListAdministration()
+
+    prosposal = user.from_dict(api.payload)
+
+    if prosposal is not None:
+      x = adm.create_user(prosposal.get_user_name(), prosposal.get_user_id())
+      return x, 200
+    else:
+        return '', 500
