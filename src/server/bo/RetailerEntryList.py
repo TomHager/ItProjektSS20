fc32d5f1de446b17a3db7dde899d8b59f4e31307
FinkRobin <rf022@hdm-stuttgart.de>
2020-06-23 12:26:32 +0200

    RetailerEntryList.py geändert!

--- src/server/bo/RetailerEntryList.py
+++ src/server/bo/RetailerEntryList.py
@@ -2,6 +2,7 @@ from server.bo import BusinessObject as bo
 from server.bo.ShoppingList import ShoppingList
 from server.bo.Retailer import Retailer
 from server.bo.User import User
+from server.bo.Entry import Entry
 
 class RetailerEntryList (bo.BusinessObject):
 
@@ -10,6 +11,7 @@ class RetailerEntryList (bo.BusinessObject):
         self.__shopping_list_id = ""
         self.__retailer_id = ""
         self.__user_id = ""
+        self.__entry_id = ""
 
     def get_shopping_list_id(self):
         return self.__shopping_list_id
@@ -29,16 +31,23 @@ class RetailerEntryList (bo.BusinessObject):
     def set_user_id(self, value):
         self.__user_id = value
 
+    def get_entry_id(self):
+        return self.__entry_id
+
+    def set_entry_id(self, value):
+        self.__entry_id = value
+
     def __str__(self):
         """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz."""
-        return "RetailerEntryList: {}, {}, {}, {}".format(self.get_id(), self.__shopping_list_id, self.__retailer_id, self.__user_id)
+        return "RetailerEntryList: {}, {}, {}, {}, {}".format(self.get_id(), self.__shopping_list_id, self.__retailer_id, self.__user_id, self.__entry_id)
 
     @staticmethod
     def from_dict(dictionary=dict()):
         """Umwandeln eines Python dict() in eine RetailerEntryList()."""
         obj = RetailerEntryList()
         obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
-        obj.set_name(dictionary["shopping_list_id"])
+        obj.set_shopping_list_id(dictionary["shopping_list_id"])
         obj.set_retailer_id(dictionary["retailer_id"])
         obj.set_retailer_id(dictionary["user_id"])
+        obj.set_entry_id(dictionary["entry_id"])
         return obj
