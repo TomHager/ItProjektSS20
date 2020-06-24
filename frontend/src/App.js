0d6b1ad6420c373f00fe45bd4b9236cdaca73e96
Tom <th116@hdm-stuttgart.de>
2020-06-24 20:15:32 +0200

    Update App Router and Added About and GroupList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,38 +2,45 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import { Container, ThemeProvider, CssBaseline } from '@material-ui/core';
 import Header from './components/layout/Header';
+import About from './components/pages/About'
+import GroupList from './components/pages/GroupList'
 // import Theme from './Theme';
 // import SignIn from './components/pages/SignIn';
 // import LoadingProgress from './components/dialogs/LoadingProgress';
 // import ContextErrorMessage from './components/dialogs/ContextErrorMessage';
 
 /**
- * The main administration app. It uses Googles firebase to log into the backend. 
+ * The main administration app. It uses Googles firebase to log into the backend.
  * For routing the user to the respective pages, react-router-dom ist used.
- * 
+ *
  * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
  * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
  * @see [react-router-dom](https://reacttraining.com/react-router/web/guides/quick-start)
- * 
+ *
  */
 class App extends React.Component {
+  /** Constructor of the app, which initializes firebase  */
+  constructor(props) {
+    super(props);
 
-	/** Constructor of the app, which initializes firebase  */
-	constructor(props) {
-		super(props);
+    // Init an empty state
+    this.state = {};
+  }
 
-		// Init an empty state
-		this.state = {
-		};
-	}
-
-	/** Renders the whole app */
-	render() {
-
-		return (
-      <Header />
-				      
-		);
-	}
+  /** Renders the whole app */
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <Header />
+        <Redirect from='/' to='groupList' />
+        <Route exact path='/groupList'>
+          <GroupList />
+        </Route>
+        <Route  path='/about'>
+            <About />
+        </Route>
+      </Router>
+    );
+  }
 }
 export default App;
---
+++ frontend/src/components/pages/GroupList.js
@@ -0,0 +1,109 @@
+// import { withRouter } from "react-router-dom";
+// import React, { Component }  from 'react';
+// // import { withStyles, Button, TextField, InputAdornment, IconButton, Grid, Typography } from '@material-ui/core';
+// // // import AddIcon from '@material-ui/icons/Add';
+// // // import ClearIcon from '@material-ui/icons/Clear'
+// // import PropTypes from 'prop-types';
+
+
+// // class GroupList extends Component {
+
+// //     constructor(props) {
+// //         super(props);
+// //         // console.log(props);
+    
+// //         this.state = {
+
+// //         }
+// //     }
+
+// //   render() {
+// //     const { classes } = this.props;
+// //     const { groupFilter } = this.state;
+
+// //     return (
+// //     //   <div className={classes.root}>
+// //     //     <Grid
+// //     //       className={classes.groupFilter}
+// //     //       container
+// //     //       spacing={1}
+// //     //       justify="flex-start"
+// //     //       alignItems="center"
+// //     //     >
+// //     //       <Grid item>
+// //     //         <Typography>Filter group list by name:</Typography>
+// //     //       </Grid>
+// //     //       <Grid item xs={4}>
+// //     //         <TextField
+// //     //           autoFocus
+// //     //           fullWidth
+// //     //           id="groupFilter"
+// //     //           type="text"
+// //     //           value={groupFilter}
+// //     //           onChange={this.filterFieldValueChange}
+// //     //           InputProps={{
+// //     //             endAdornment: (
+// //     //               <InputAdornment position="end">
+// //     //                 <IconButton onClick={this.clearFilterFieldButtonClicked}>
+// //     //                   {/* <ClearIcon /> */}
+// //     //                 </IconButton>
+// //     //               </InputAdornment>
+// //     //             ),
+// //     //           }}
+// //     //         />
+// //     //       </Grid>
+// //     //     </Grid>
+// //     //   </div>
+// //         <div>
+// //             <table>
+// //                 <th>hallo</th>
+// //             </table>
+// //         </div>
+// //     );
+// //   }
+// // }
+
+// // /** PropTypes */
+// // GroupList.propTypes = {
+// //     /** @ignore */
+// //     classes: PropTypes.object.isRequired,
+// //     /** @ignore */
+// //     location: PropTypes.object.isRequired,
+// //   }
+
+// export default withRouter(GroupList);
+// // // withRouter(withStyles(styles)(GroupList));
+
+import React from "react";
+import { makeStyles, Paper, Typography, Link } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(1),
+  },
+  content: {
+    margin: theme.spacing(1),
+  },
+}));
+
+/**
+ * Shows the about page with the impressum
+ */
+function GroupList() {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={0} className={classes.root}>
+      <div className={classes.content}>
+        <Typography variant="h6">KEIN Project</Typography>
+        <br />
+        <Typography>Writen by NULL</Typography>
+      </div>
+    </Paper>
+  );
+}
+
+export default GroupList;
