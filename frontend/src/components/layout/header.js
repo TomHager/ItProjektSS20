d733083932198f74d9f9704baece31a12c517594
LukasRutkauskas <lr057@hdm-stuttgart.de>
2020-06-24 18:26:36 +0200

    Merge dev into LukasRutkauskas

--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Paper, Typography, Tabs, Tab } from "@material-ui/core";
-import { Link as RouterLink } from "react-router-dom";
-import ProfileDropDown from "../dialogs/ProfileDropDown";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Paper, Typography, Tabs, Tab } from '@material-ui/core';
+// import { Link as RouterLink } from 'react-router-dom';
 
 /**
  * Shows the header with the main navigation Tabs within a Paper.
- *
+ * 
  * @see See Material-UIs [Tabs](https://material-ui.com/components/tabs/)
  * @see See Material-UIs [Paper](https://material-ui.com/components/paper/)
- *
- * @author [Christoph Kunz](https://github.com/christophkunz)
+ * 
  */
 class Header extends Component {
+
   constructor(props) {
     super(props);
 
     // Init an empty state
     this.state = {
-      tabindex: 0,
+      tabindex: 0
     };
   }
 
@@ -26,45 +25,43 @@ class Header extends Component {
   handleTabChange = (e, newIndex) => {
     // console.log(newValue)
     this.setState({
-      tabindex: newIndex,
-    });
+      tabindex: newIndex
+    })
   };
 
   /** Renders the component */
   render() {
-    const { user } = this.props;
+    // const { user } = this.props;
 
     return (
-      <Paper variant="outlined">
-        <ProfileDropDown user={user} />
-        <Typography variant="h3" component="h1" align="center">
-          HdM Bank Administration
+      <Paper variant='outlined'>
+        {/* <ProfileDropDown user={user} /> */}
+        <Typography variant='h3' component='h1' align='center'>
+          <b>iKauf</b> your futuristic Shoppingapp 
         </Typography>
-        <Typography variant="h4" component="h2" align="center">
-          Client Advisor Home
+        <Typography variant='h4' component='h2' align='center'>
+          <u>Overview</u>
         </Typography>
-        {user ? (
-          <Tabs
-            indicatorColor="primary"
-            textColor="primary"
-            centered
-            value={this.state.tabindex}
-            onChange={this.handleTabChange}
-          >
-            <Tab label="Customers" component={RouterLink} to={`/customers`} />
-            <Tab label="All Accounts" component={RouterLink} to={`/accounts`} />
-            <Tab label="About" component={RouterLink} to={`/about`} />
-          </Tabs>
-        ) : null}
+        {
+        //   user ?
+            <Tabs indicatorColor='primary' textColor='primary' centered value={this.state.tabindex} onChange={this.handleTabChange} >
+                 {/* component={RouterLink} to={`/customers`}  */}
+              <Tab label='Groups'/>
+              <Tab label='My Shoppinglists'/>
+              <Tab label='Profile'/>
+              <Tab label='About'/>
+            </Tabs>
+            // : null
+        }
       </Paper>
-    );
+    )
   }
 }
 
 /** PropTypes */
-Header.propTypes = {
-  /** The logged in firesbase user */
-  user: PropTypes.object,
-};
+// Header.propTypes = {
+//   /** The logged in firesbase user */
+//   user: PropTypes.object,
+// }
 
 export default Header;
