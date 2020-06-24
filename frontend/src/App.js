5738e26ad5d3feb8f2ba3f82e705c4be1232b544
Tom <th116@hdm-stuttgart.de>
2020-06-24 18:05:14 +0200

    Added Main App.js and Header.js class for Frontend
    App.js; Header.js; Index.js; package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Container, ThemeProvider, CssBaseline } from '@material-ui/core';
+import Header from './components/layout/Header';
+// import Theme from './Theme';
+// import SignIn from './components/pages/SignIn';
+// import LoadingProgress from './components/dialogs/LoadingProgress';
+// import ContextErrorMessage from './components/dialogs/ContextErrorMessage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * The main administration app. It uses Googles firebase to log into the backend. 
+ * For routing the user to the respective pages, react-router-dom ist used.
+ * 
+ * @see See Google [firebase.auth()](https://firebase.google.com/docs/reference/js/firebase.auth.Auth)
+ * @see See Google [firebase.auth().signInWithRedirect](https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signinwithredirect)
+ * @see [react-router-dom](https://reacttraining.com/react-router/web/guides/quick-start)
+ * 
+ */
+class App extends React.Component {
+
+	/** Constructor of the app, which initializes firebase  */
+	constructor(props) {
+		super(props);
+
+		// Init an empty state
+		this.state = {
+		};
+	}
+
+	/** Renders the whole app */
+	render() {
+
+		return (
+      <Header />
+				      
+		);
+	}
 }
-
 export default App;
