0835bc35bb0a63c80e117d61736d7a4cce3428f7
LukasRutkauskas <lr057@hdm-stuttgart.de>
2020-06-24 20:20:17 +0200

    Changes

--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -1,22 +1,23 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Paper, Typography, Tabs, Tab } from "@material-ui/core";
-import { Link as RouterLink } from "react-router-dom";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Paper, Typography, Tabs, Tab } from '@material-ui/core';
+// import { Link as RouterLink } from 'react-router-dom';
 
 /**
  * Shows the header with the main navigation Tabs within a Paper.
- *
+ * 
  * @see See Material-UIs [Tabs](https://material-ui.com/components/tabs/)
  * @see See Material-UIs [Paper](https://material-ui.com/components/paper/)
- *
+ * 
  */
 class Header extends Component {
+
   constructor(props) {
     super(props);
 
     // Init an empty state
     this.state = {
-      tabindex: 0,
+      tabindex: 0
     };
   }
 
@@ -24,8 +25,8 @@ class Header extends Component {
   handleTabChange = (e, newIndex) => {
     // console.log(newValue)
     this.setState({
-      tabindex: newIndex,
-    });
+      tabindex: newIndex
+    })
   };
 
   /** Renders the component */
@@ -33,33 +34,27 @@ class Header extends Component {
     // const { user } = this.props;
 
     return (
-      <Paper variant="outlined">
+      <Paper variant='outlined'>
         {/* <ProfileDropDown user={user} /> */}
-        <Typography variant="h3" component="h1" align="center">
-          <b>iKauf</b> your futuristic Shoppingapp
+        <Typography variant='h3' component='h1' align='center'>
+          <b>iKauf</b> your futuristic Shoppingapp 
         </Typography>
-        <Typography variant="h4" component="h2" align="center">
+        <Typography variant='h4' component='h2' align='center'>
           <u>Overview</u>
         </Typography>
         {
-          //   user ?
-          <Tabs
-            indicatorColor="primary"
-            textColor="primary"
-            centered
-            value={this.state.tabindex}
-            onChange={this.handleTabChange}
-          >
-            component={RouterLink} to={`/`}
-            <Tab label="Groups" />
-            <Tab label="My Shoppinglists" />
-            <Tab label="Profile" />
-            <Tab label="About" />
-          </Tabs>
-          // : null
+        //   user ?
+            <Tabs indicatorColor='primary' textColor='primary' centered value={this.state.tabindex} onChange={this.handleTabChange} >
+                 {/* component={RouterLink} to={`/customers`}  */}
+              <Tab label='Groups'/>
+              <Tab label='My Shoppinglists'/>
+              <Tab label='Profile'/>
+              <Tab label='About'/>
+            </Tabs>
+            // : null
         }
       </Paper>
-    );
+    )
   }
 }
 
