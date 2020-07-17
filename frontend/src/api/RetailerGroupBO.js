b9b44d001114e72d8937a0ed31c1c6fcb5c49872
TomHager <th116@hdm-stuttgart.de>
2020-07-17 15:33:14 +0200

    Update Frontend API
    Added 2 BOs and all Links in ShoppingAPI

---
+++ frontend/src/api/RetailerGroupBO.js
@@ -0,0 +1,60 @@
+import BusinessObject from './BusinessObject';
+
+/**
+ * @author Tom Hager
+ * @author Christoph Kunz
+ */
+
+export default class RetailerGroupBO extends BusinessObject {
+  constructor(retailerMember, retailerGroup) {
+    super();
+    this.retailer_member = retailerMember;
+    this.retailer_group = retailerGroup;
+  }
+
+  getRetailerMember() {
+    return this.retailer_member;
+  }
+
+  setRetailerMember(retailerMember) {
+    this.retailer_member = retailerMember;
+  }
+
+  getRetailerGroup() {
+    return this.retailer_group;
+  }
+
+  setRetailerGroup(retailerGroup) {
+    this.retailer_group = retailerGroup;
+  }
+
+  /*
+    Nutzungsmöglichkeit:
+
+        let outputHTML = '';
+        customers = Customer.fromJSON(this.responseText);
+        customers.forEach((c) => {
+            outputHTML += '<div class='customer'>' + c.getFirstName() + ' ' + c.getLastName() + '</div>';
+        });
+
+    */
+
+  // Returns an Array of CustomerBOs from a given JSON structure
+  static fromJSON(groups) {
+    let result = [];
+
+    if (Array.isArray(groups)) {
+      groups.forEach((c) => {
+        Object.setPrototypeOf(c, RetailerGroupBO.prototype);
+        result.push(c);
+      });
+    } else {
+      // Es handelt sich offenbar um ein singuläres Objekt
+      let c = groups;
+      Object.setPrototypeOf(c, RetailerGroupBO.prototype);
+      result.push(c);
+    }
+
+    return result;
+  }
+}
