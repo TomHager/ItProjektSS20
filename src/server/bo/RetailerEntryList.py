20541e21e132868bbf9b8c09d12b16030be93678
FinkRobin <rf022@hdm-stuttgart.de>
2020-07-20 20:50:06 +0200

    Entry.py, EntryMapper.py erweitert

--- src/server/bo/RetailerEntryList.py
+++ src/server/bo/RetailerEntryList.py
@@ -1,4 +1,3 @@
-from server.bo import BusinessObject as bo
 from server.bo import ShoppingList as shoppinglist
 from server.bo import Retailer as retailer
 from server.bo import User as user
@@ -7,17 +6,16 @@ from server.bo import Entry as entry
 """ 
 @author Tom Hager
  """
- 
-class RetailerEntryList (shoppinglist.ShoppingList, retailer.Retailer, user.User, entry.Entry):
+
+
+class RetailerEntryList(shoppinglist.ShoppingList, retailer.Retailer, user.User, entry.Entry):
 
     def __init__(self):
         super().__init__()
-        self.shopping_list_id = " "
-        self.entry_id = " "
-        self.user_id = " "
-        self.retailer_id = " "
-
-
+        self.shopping_list_id = ""
+        self.entry_id = ""
+        self.user_id = ""
+        self.retailer_id = ""
 
     def get_shopping_list_id(self):
         return self._shopping_list_id
@@ -45,14 +43,13 @@ class RetailerEntryList (shoppinglist.ShoppingList, retailer.Retailer, user.User
 
     def __str__(self):
         """Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz."""
-        return "RetailerEntryList: {}, {}, {}, {}, {}".format(self.get_id(), self._shopping_list_id,
-                                                              self._entry_id, self._user_id, self._retailer_id)
+        return "RetailerEntryList: {}, {}, {}, {}".format(self._shopping_list_id, self._entry_id, self._user_id,
+                                                          self._retailer_id)
 
     @staticmethod
     def from_dict(dictionary=dict()):
         """Umwandeln eines Python dict() in eine RetailerEntryList()."""
         obj = RetailerEntryList()
-        obj.set_id(dictionary["id"])  # eigentlich Teil von BusinessObject !
         obj.set_shopping_list_id(dictionary["shopping_list_id"])
         obj.set_entry_id(dictionary["entry_id"])
         obj.set_user_id(dictionary["user_id"])
